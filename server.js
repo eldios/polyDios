f1b0c776655f589012834873bb6f06c32537b797
Lele <lele@amicofigo.com>
2011-08-26 14:22:09 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+var HTMLparser = require('../ht/htmlToText'),
+    exec = require('child_process').exec ,
+    async = require('async'),
+    exports = module.exports ;
+
+var findGrm = exports.findGrm = function(grmName, cbk){
+  exec('find . -iname \'*'+grmName+'*grm\' -type f' ,
+    function (err, stdout, stderr){
+      stdout = stdout.replace(/^\s+|\s+$/g,'').split("\n")[0];
+      if (cbk != undefined && cbk != null){
+        console.log('cbk:' + cbk);
+        cbk(stdout);
+      } else {
+        console.log('undefined cbk:' + cbk);
+        return stdout;
+      };
+  });
+}
+
+var callPolygen = exports.callPolygen = function(grm){
+  var grm_file ;
+  async.series([
+    function(cbk) {
+      cbk(null,findGrm(grm,console.log));
+    } ,
+    function(cbk) {
+      exec('bin/polygen ' + grm_file ,
+        function (error, stdout, stderr) {
+          stdout = HTMLparser.htmlToText(stdout);
+          cbk(null,stdout);
+        });
+    }
+  ]);
+};
+
+var listGrms = exports.listGrms = function(cbk){
+  exec('find . -iname \'*grm\' -type f' ,
+    function (error, stdout, stderr){
+      console.log('cbk:' + cbk);
+      if (cbk != undefined && cbk != null){
+              console.log('cbk:' + cbk);
+        cbk(stdout.replace(/^\s+|\s+$/g, '').split("\n"));
+      } else {
+              console.log('cbk:' + cbk);
+        return stdout.replace(/^\s+|\s+$/g, '').split("\n");
+      };
+  });
+};
+
+var helpOpt = process.ARGV.indexOf('-h') ;
+if ( helpOpt > -1 ) {
+  console.log(
+    ['***polygen nodeJS wrapper***',
+    '-l      : list available Grammars',
+    '-x grm  : exec polygen on grm file',
+    '-h      : print this help'].join("\n"));
+  process.exit();
+};
+
+var listOpt = process.ARGV.indexOf('-l') ;
+if ( listOpt > -1 ) {
+  listGrms(console.log);
+  process.exit();
+};
+
+var callOpt = process.ARGV.indexOf('-x') ; 
+if ( callOpt > -1 ) {
+  callPolygen(process.ARGV[callOpt+1]);
+  process.exit();
+};
